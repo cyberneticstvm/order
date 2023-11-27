09dc7d6ba202268554b2c73fe2cd1d70bb87c4b5
Cybernetics <mail@cybernetics.me>
2023-11-27 10:18:27 +0530

    small updates

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -35,59 +35,6 @@
         </div>
     </div>
     <!-- Container-fluid Ends-->
-    <!-- Container-fluid starts-->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col">
-                                <h5>Patient Register</h5><span>Patient Management</span>
-                            </div>
-                            <div class="col text-end"><a href="{{ route('patient.create', ['type' => 'Direct', 'type_id' => '0']) }}" class="btn btn-primary" type="button">Add New</a></div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive theme-scrollbar">
-                            <table class="display" id="dataTable" style="width:100%">
-                                <thead>
-                                    <tr>
-                                        <th>SL No</th>
-                                        <th>Patient Name</th>
-                                        <th>Patient ID</th>
-                                        <th>Mobile</th>
-                                        <th>Place</th>
-                                        <th>Review</th>
-                                        <th>Status</th>
-                                        <th>Edit</th>
-                                        <th>Delete</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse($patients as $key => $patient)
-                                    <tr>
-                                        <td>{{ $key + 1 }}</td>
-                                        <td>{{ $patient->name }}</td>
-                                        <td>{{ $patient->patient_id }}</td>
-                                        <td>{{ $patient->mobile }}</td>
-                                        <td>{{ $patient->place }}</td>
-                                        <td class="text-center"><a href="/backend/consultation/create/{{ encrypt($patient->id) }}">Review</a></td>
-                                        <td>{!! $patient->status() !!}</td>
-                                        <td class="text-center"><a href="{{ route('patient.edit', encrypt($patient->id)) }}"><i class="fa fa-edit text-muted fa-lg"></i></a></td>
-                                        <td class="text-center"><a href="{{ route('patient.delete', encrypt($patient->id)) }}" class="dlt"><i class="fa fa-trash text-danger fa-lg"></i></a></td>
-                                    </tr>
-                                    @empty
-                                    @endforelse
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Container-fluid Ends-->
 </div>
 @if(!Session::has('branch'))
 <div class="modal fade" id="branchSelector" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="branchSelector" aria-hidden="true">
