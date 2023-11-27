1da04b98c1d6052a40fab94dd86292f5f144b895
Cybernetics <mail@cybernetics.me>
2023-11-27 10:16:25 +0530

    small updates

--- resources/views/backend/nav.blade.php
+++ resources/views/backend/nav.blade.php
@@ -25,49 +25,12 @@
             </a>
             <ul class="sidebar-submenu">
                 <li><a class="" href="{{ route('dashboard') }}">Patient Register</a></li>
-                <li><a href="{{ route('consultations') }}">Consultation Register</a></li>
-                <li><a href="{{ route('appointment.list') }}">Today's Appointment</a></li>
-                <li><a class="" href="{{ route('store.order') }}">Order Register</a></li>
-                <li><a class="" href="{{ route('pharmacy.order') }}">Pharmacy Register</a></li>
-                <li><a class="" href="{{ route('search') }}">Search</a></li>
             </ul>
         </li>
         <li class="sidebar-main-title">
             <div>
                 <h6 class="">Operations</h6>
             </div>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#stroke-contact') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#fill-contact') }}"></use>
-                </svg>
-                <span>Appointment</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('appointments') }}">Appointment Register</a></li>
-                <li><a href="{{ route('appointment.list') }}">Today's Appointment</a></li>
-            </ul>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#stroke-to-do') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#fill-to-do') }}"></use>
-                </svg>
-                <span>Consultation</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('patients') }}">Patient Register</a></li>
-                <li><a href="{{ route('consultations') }}">Consultation Register</a></li>
-                <li><a href="{{ route('mrecords') }}">Medical Record Register</a></li>
-                <li><a href="{{ route('patient.procedures') }}">Patient Procedure Register</a></li>
-            </ul>
         </li>
         <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
             <a class="sidebar-link sidebar-title" href="javascript:void(0)">
@@ -96,77 +59,6 @@
             </a>
             <ul class="sidebar-submenu">
                 <li><a href="{{ route('branches') }}">Branch Register</a></li>
-            </ul>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#stroke-user') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#fill-user') }}"></use>
-                </svg>
-                <span>Doctor Management</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('doctors') }}">Doctor Register</a></li>
-            </ul>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#stroke-file') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#fill-file') }}"></use>
-                </svg>
-                <span>Procedure Management</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('procedures') }}">Procedure Register</a></li>
-            </ul>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#stroke-widget') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#fill-widget') }}"></use>
-                </svg>
-                <span>Camp</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('camps') }}">Camp Register</a></li>
-            </ul>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#doller-return') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#doller-return') }}"></use>
-                </svg>
-                <span>Income & Expenses</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('heads') }}">Heads</a></li>
-                <li><a href="{{ route('iande') }}">Income & Expense Register</a></li>
-            </ul>
-        </li>
-        <li class="sidebar-list"><i class="fa fa-thumb-tack"> </i>
-            <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                <svg class="stroke-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#stroke-knowledgebase') }}"></use>
-                </svg>
-                <svg class="fill-icon">
-                    <use href="{{ asset('/backend/assets/svg/icon-sprite.svg#fill-knowledgebase') }}"></use>
-                </svg>
-                <span>Documents</span>
-            </a>
-            <ul class="sidebar-submenu">
-                <li><a href="{{ route('documents') }}">Document Register</a></li>
             </ul>
         </li>
         <li class="sidebar-main-title">
