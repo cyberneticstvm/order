8f7e14aa9f7c68154681c6a3ca0f808afb3babf3
Cybernetics <mail@cybernetics.me>
2024-02-24 10:58:05 +0530

    small updates

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
-    //
+    public function settings()
+    {
+        return redirect()->back()->with("error", "Nothing found");
+    }
 }
