fc91382a8d8fd343fde7dcffbd3dcfdd2d8440c9
Cybernetics <mail@cybernetics.me>
2024-03-04 12:16:02 +0530

    small updates

--- resources/views/backend/pdf/store-order-receipt.blade.php
+++ resources/views/backend/pdf/store-order-receipt.blade.php
@@ -82,7 +82,87 @@
     </div>-->
 </div>
 <hr style="border: 1px dotted;">
-<div class="row">
-
+<div class="row mt-30">
+    <div class="col">
+        <table class="table" width="50%" cellpadding="0" cellspacing="0">
+            <thead>
+                <tr>
+                    <td widtd="20%" class="no-border">Customer Name</td>
+                    <td widtd="25%" class="no-border">{{ $order->name }}</td>
+                    <td widtd="10%" class="no-border">Address</td>
+                    <td widtd="45%" class="no-border">{{ $order->place }}</td>
+                </tr>
+                <tr>
+                    <td widtd="20%" class="no-border">Product Adviser</td>
+                    <td widtd="25%" class="no-border">{{ $order->adviser->name }}</td>
+                    <td widtd="10%" class="no-border">Order Number</td>
+                    <td widtd="45%" class="no-border">{{ $order->branch->code }}/{{ $order->id }}</td>
+                </tr>
+                <tr>
+                    <td widtd="20%" class="no-border">MRN</td>
+                    <td widtd="25%" class="no-border">{{ $order->consultation_id }}</td>
+                    <td widtd="10%" class="no-border">Order Date</td>
+                    <td widtd="45%" class="no-border">{{ $order->created_at->format('d, M Y') }}</td>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+<div class="row mt-30">
+    <div class="col">
+        <table class="bordered" width="50%" cellspacing="0" cellpadding="0">
+            <thead>
+                <tr>
+                    <th class="no-border">SL No</th>
+                    <th class="no-border">Eye</th>
+                    <th class="no-border">Product</th>
+                    <th class="no-border">Qty</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $slno = 1; @endphp
+                @forelse($order->details as $key => $value)
+                <tr>
+                    <td class="no-border">{{ $slno++ }}</td>
+                    <td class="no-border">{{ $value->eye }}</td>
+                    <td class="no-border">{{ $value->product->name }}</td>
+                    <td class="text-right no-border">{{ $value->qty }}</td>
+                </tr>
+                @empty
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="row mt-30">
+    <div class="col">
+        <table class="no-border" cellpadding="0" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th class="no-border"></th>
+                    <th class="no-border">SPH</th>
+                    <th class="no-border">CYL</th>
+                    <th class="no-border">AXIS</th>
+                    <th class="no-border">ADD</th>
+                    <th class="no-border">IPD</th>
+                    <th class="no-border">FH</th>
+                    <th class="no-border">PRISM</th>
+                </tr>
+                @forelse($order->details->whereIn('eye', ['re', 'le']) as $key => $value)
+                <tr>
+                    <td class="no-border">{{ $value->eye }}</td>
+                    <td class="no-border">{{ $value->sph }}</td>
+                    <td class="no-border">{{ $value->cyl }}</td>
+                    <td class="no-border">{{ $value->axis }}</td>
+                    <td class="no-border">{{ $value->add }}</td>
+                    <td class="no-border">{{ $value->ipd }}</td>
+                    <td class="no-border"></td>
+                    <td class="no-border"></td>
+                </tr>
+                @empty
+                @endforelse
+            </thead>
+        </table>
+    </div>
 </div>
 @endsection
