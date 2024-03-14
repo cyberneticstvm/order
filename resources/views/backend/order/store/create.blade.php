2ad738ade3b5a0f2602221229a089589f6bd0647
Cybernetics <mail@cybernetics.me>
2024-03-14 23:32:40 +0530

    small updates

--- resources/views/backend/order/store/create.blade.php
+++ resources/views/backend/order/store/create.blade.php
@@ -190,7 +190,7 @@
                                                     <td>
                                                         {{ html()->select('int_add[]', $powers?->where('name', 'add')->pluck('value', 'value'), $spectacle?->le_int_add ?? $powers?->where('name', 'intad')->where('default', 'true')?->first()?->value)->class('border-0 select2')->attribute('id', 'intadd1') }}
                                                     </td>
-                                                    <td><input type="text" name='ipd[]' class="w-100 border-0 text-center" placeholder="IPD" maxlength="6" /></td>
+                                                    <td><input type="text" name='ipd[]' class="w-100 border-0 text-center" placeholder="IPD" value="{{ $spectacle?->ipd ?? '' }}" maxlength="6" /></td>
                                                     <td>
                                                         {{ html()->select('product_id[]', $products->where('category', 'lens')->pluck('name', 'id'), old('product_id'))->class('border-0 select2 selPdct pdctSecond')->attribute('data-batch', 'NA')->attribute('data-category', 'lens')->attribute('id', 'cat-lens2')->placeholder('Select')->required() }}
                                                     </td>
