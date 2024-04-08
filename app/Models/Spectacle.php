46e980946dff8ec312fa5501ad22110219b98301
Cybernetics <mail@cybernetics.me>
2024-04-08 13:44:39 +0530

    small updates

--- app/Models/Spectacle.php
+++ app/Models/Spectacle.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -30,5 +31,12 @@ class Spectacle extends Model
     public function customer()
     {
         return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+
+    public function hasOrder($specid)
+    {
+        //$ocount = Order::where('customer_id', $cid)->whereDate('created_at', Carbon::today())->count('id');
+        $spec = Spectacle::where('id', $specid)->whereDate('created_at', Carbon::today())->first();
+        return ($spec?->order_id) ? true : false;
     }
 }
