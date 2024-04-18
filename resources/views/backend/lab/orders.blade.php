d4ae0ad30819b5398b5e8d29fc834eb0cb496228
Cybernetics <mail@cybernetics.me>
2024-04-18 10:07:45 +0530

    small updates

--- resources/views/backend/lab/orders.blade.php
+++ resources/views/backend/lab/orders.blade.php
@@ -81,7 +81,7 @@
                                         <td>{{ $item->order?->fh }}</td>
                                         <td>{{ $item->order?->ed }}</td>
                                         <td>{{ $item->order?->special_lab_note }}</td>
-                                        <td>{{ getFrameType($item->order->id) }}</td>
+                                        <td>{{ getFrameType($item->order?->id ?? 0) }}</td>
                                         <td>{{ $item->order?->name }}</td>
                                     </tr>
                                     @empty
