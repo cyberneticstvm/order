6b074646f52e53a465c198c4174126bee379e8c5
Cybernetics <mail@cybernetics.me>
2024-04-21 09:06:13 +0530

    small updates

--- resources/views/backend/settings/stock-adjustment.blade.php
+++ resources/views/backend/settings/stock-adjustment.blade.php
@@ -26,7 +26,7 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="card-wrapper">
-                            <form class="row g-3" method="post" action="{{ route('report.stock.status.fetch') }}">
+                            <form class="row g-3" method="post" action="{{ route('setting.stock.adjustment.fetch') }}">
                                 @csrf
                                 <div class="col-md-3">
                                     <label class="form-label req">Product Category</label>
