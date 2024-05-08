70f8ec9568acd953447867247f63212ecbf0fcfc
Cybernetics <mail@cybernetics.me>
2024-05-08 10:44:36 +0530

    small updates

--- resources/views/backend/product/import/lens.blade.php
+++ resources/views/backend/product/import/lens.blade.php
@@ -42,7 +42,7 @@
                                     <a href="{{ asset('/backend/assets/docs/Lenses.xlsx') }}">Download Format</a>
                                 </div>
                                 <div class="col-md-12">
-                                    <p>Last Id is: L{{ getLastId('lens') }}</p>
+                                    <h5 class="text-info">Last Id is: L{{ getLastId('lens') }}</h5>
                                 </div>
                             </form>
                         </div>
