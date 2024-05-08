a0591e9df3119b5d16d4d070cd731d6b566701d8
Cybernetics <mail@cybernetics.me>
2024-05-08 11:22:50 +0530

    small updates

--- resources/views/backend/message.blade.php
+++ resources/views/backend/message.blade.php
@@ -81,7 +81,7 @@
             url: '/ajax/check/pending/transfers',
             type: 'get',
             success: function(response) {
-                console(response)
+                console.log(response)
                 error(response);
             }
         });
