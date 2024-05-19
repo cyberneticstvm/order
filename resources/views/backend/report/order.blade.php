47c20156593835ac2ec57a96a7c5c385ba8756b3
Cybernetics <mail@cybernetics.me>
2024-05-19 12:05:48 +0530

    small updates

--- resources/views/backend/report/order.blade.php
+++ resources/views/backend/report/order.blade.php
@@ -26,7 +26,7 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="card-wrapper">
-                            <form class="row g-3" method="post" action="{{ route('report.sales.fetch') }}">
+                            <form class="row g-3" method="post" action="{{ route('report.order.fetch') }}">
                                 @csrf
                                 <input type="hidden" name="redirect" value="order" />
                                 <div class="col-md-2">
