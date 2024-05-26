63885ea3d7f87c109441152f25d0b148786e337f
Cybernetics <mail@cybernetics.me>
2024-05-26 17:39:53 +0530

    small updates

--- resources/views/backend/base.blade.php
+++ resources/views/backend/base.blade.php
@@ -72,6 +72,7 @@
         </div>
         <div class="nav-right col-xxl-7 col-xl-6 col-md-7 col-8 pull-right right-header p-0 ms-auto">
           <ul class="nav-menus">
+            @if(Session::has('branch'))
             <li class="language-nav">
               <div class="translate_wrapper">
                 <div class="current_lang">
@@ -85,6 +86,7 @@
                 </div>
               </div>
             </li>
+            @endif
             <li class="profile-nav onhover-dropdown pe-0 py-0">
               <div class="media profile-media"><img class="b-r-10" src="{{ asset('/backend/assets/images/dashboard/profile.png') }}" alt="">
                 <div class="media-body"><span>{{ Auth::user()->name }}</span>
