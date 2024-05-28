2a61c7551b6de8d42908602f1fe711b693ce746a
Vijoy Sasidharan <mail@cybernetics.me>
2024-05-28 14:48:03 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -372,12 +372,13 @@ class AjaxController extends Controller
 
     public function getBookedProductDetails(Request $request)
     {
+        $c = 1;
         $type = ($request->category == 'lens') ? ['re', 'le'] : [$request->category];
         $orders = OrderDetail::leftJoin('orders as o', 'o.id', 'order_details.order_id')->where('o.branch_id', $request->branch)->selectRaw("order_details.*")->whereNotIn('o.order_status', ['delivered', 'cancelled'])->whereIn('order_details.eye', $type)->whereNull('order_details.return')->whereNull('o.deleted_at')->get();
         $op = "<table class='table table-bordered'><thead><tr><th>SL No</th><th>Order No</th><th>Product</th><th>Qty</th><th>Status</th></tr></thead><tbody>";
         foreach ($orders as $key => $item) :
             $op .= "<tr>";
-            $op .= "<td>" . $key + 1 . "</td>";
+            $op .= "<td>" . $c++ . "</td>";
             $op .= "<td>" . $item->order?->ono() . "</td>";
             $op .= "<td>" . $item->product?->name . "</td>";
             $op .= "<td class='text-end'>" . $item->qty . "</td>";
