b50200bb5d0c0ae7200064787bd8b49dbb706880
Vijoy Sasidharan <mail@cybernetics.me>
2024-05-28 17:13:26 +0530

    small updates

--- resources/views/backend/lab/orders.blade.php
+++ resources/views/backend/lab/orders.blade.php
@@ -73,7 +73,7 @@
                                         </td>
                                         <td class="text-center"><a href="javascript:void(0)" class="labNote" data-drawer="labNoteDrawer" data-oid="{{ $item->order_id }}"><i class="fa fa-edit {{ isOrderNote($item->order?->id) }} fa-lg"></i></a></td>
                                         <td class="{{ ($item->lab_id == '0') ? 'text-warning' : ''  }}">{{ $item->created_at->format('d.M.Y') }}</td>
-                                        <td><a href="javascript:void(0)" class="dayBook {{ $item->order?->isEdited() ?? 'text-primary' }}" data-from-date="" data-to-date="" data-type="order" data-mode="{{ $item->order?->id }}" data-branch="" data-drawer="orderDrawer">{{ $item->order?->ono() }}</a></td>
+                                        <td><a href="javascript:void(0)" class="dayBook {{ $item->order?->isEdited() ?? 'text-success' }}" data-from-date="" data-to-date="" data-type="order" data-mode="{{ $item->order?->id }}" data-branch="" data-drawer="orderDrawer">{{ $item->order?->ono() }}</a></td>
                                         <td class="fw-bold">{{ strtoupper($item->eye) }}</td>
                                         <td>{{ $item->sph }}</td>
                                         <td>{{ $item->cyl }}</td>
