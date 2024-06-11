9462bcd5d4ee1d999220753906d278dab1014802
Cybernetics <mail@cybernetics.me>
2024-06-11 13:13:07 +0530

    small updates

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -39,7 +39,7 @@ class HelperController extends Controller
 
     public function viewArr($id)
     {
-        $odetail = OrderDetail::where('order_id', $id)->pluck('product_id', 'sph', 'cyl', 'axis', 'product_id')->get();
+        $odetail = OrderDetail::where('order_id', $id)->get()->pluck('product_id', 'sph', 'cyl', 'axis', 'product_id');
         dd($odetail);
     }
 
