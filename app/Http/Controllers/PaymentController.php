1e69d2a74dc11abf6ed7c92fc5e883e668d6639e
Cybernetics <mail@cybernetics.me>
2024-06-21 18:05:12 +0530

    small updates

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -79,6 +79,8 @@ class PaymentController extends Controller
             $credit = $tot->credit_used ?? 0;
             $due = $tot->invoice_total - ($paid + $credit);
             if ($request->payment_type == 'balance') :
+                dd($request->amount, $due);
+                die;
                 if ($request->amount != $due) :
                     //throw new Exception("Balance amount should be " . $due);
                     throw new Exception("Bad");
