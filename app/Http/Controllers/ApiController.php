1cd066f4c7e784ff51d5a2f4142c59c1856e4a06
Cybernetics <mail@cybernetics.me>
2024-07-03 15:07:08 +0530

    small updates

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,7 +14,7 @@ class ApiController extends Controller
         if ($secret == apiSecret()) :
             $order = Order::find($id);
             if ($order) :
-                $odetail = OrderDetail::leftJoin('products p', 'p.id', 'order_details.product_id')->select('p.name', 'order_details.qty, order_details.sph, order_details.cyl, order_details.axis, `order_details.add`')->where('order_details.order_id', $order->id)->get();
+                $odetail = OrderDetail::leftJoin('products p', 'p.id', 'order_details.product_id')->selectRaw('p.name', 'order_details.qty, order_details.sph, order_details.cyl, order_details.axis, order_details.add')->where('order_details.order_id', $order->id)->get();
                 $branch = Branch::where('id', $order->branch_id)->first();
                 return response()->json([
                     'status' => true,
