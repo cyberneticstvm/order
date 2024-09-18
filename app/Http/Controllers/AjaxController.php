59661254711e8c02a5c8cdbf42cee1d62c5c4313
Cybernetics <mail@cybernetics.me>
2024-09-18 16:28:58 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -373,11 +373,18 @@ class AjaxController extends Controller
     public function getOrderComparisonData($bid)
     {
         $bid = ($bid > 0) ? $bid : Session::get('branch');
-        $orders = Month::leftJoin('orders as o', function ($q) use ($bid) {
+        $del = Month::leftJoin('orders as o', function ($q) use ($bid) {
             $q->on('o.created_at', '>=', DB::raw('LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH'));
             $q->on('o.created_at', '<', DB::raw('LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH + INTERVAL 1 MONTH'))->where('o.branch_id', $bid);
-        })->select(DB::raw("LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH AS date, CASE WHEN order_status = 'delivered' THEN COUNT(o.id) ELSE 0 END AS dcount, COUNT(o.id) AS order_count, CONCAT_WS('/', DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%b'), DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%y')) AS month"))->groupBy('months.id', 'date')->orderByDesc('date')->get();
-        return json_encode($orders);
+        })->select(DB::raw("LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH AS date, CASE WHEN order_status = 'delivered' THEN COUNT(o.id) ELSE 0 END AS dcount"))->groupBy('months.id')->orderByDesc('date')->get();
+        $ord = Month::leftJoin('orders as o', function ($q) {
+            $q->on('o.created_at', '>=', DB::raw('LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH'));
+            $q->on('o.created_at', '<', DB::raw('LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH + INTERVAL 1 MONTH'))->where('o.branch_id', Session::get('branch'));
+        })->select(DB::raw("LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH AS date, COUNT(o.id) AS order_count, CONCAT_WS('/', DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%b'), DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%y')) AS month"))->groupBy('date', 'months.id')->orderByDesc('date')->get();
+        return json_encode([
+            'del' => $del,
+            'ord' => $ord
+        ]);
     }
 
     public function getBranches($id)
