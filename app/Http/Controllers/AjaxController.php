5f0ffdeed5b21f3dd2b9127afbbf848e91f0a0a7
Cybernetics <mail@cybernetics.me>
2024-09-18 09:29:35 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -370,17 +370,19 @@ class AjaxController extends Controller
         return json_encode($orders);
     }
 
-    public function getOrderComparisonData($bid){
+    public function getOrderComparisonData($bid)
+    {
         $bid = ($bid > 0) ? $bid : Session::get('branch');
-        $orders = Month::leftJoin('orders as o', function ($q) use($bid) {
+        $orders = Month::leftJoin('orders as o', function ($q) use ($bid) {
             $q->on('o.created_at', '>=', DB::raw('LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH'));
             $q->on('o.created_at', '<', DB::raw('LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH + INTERVAL 1 MONTH'))->where('o.branch_id', $bid);
-        })->select(DB::raw("LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH AS date, COUNT(o.id) AS order_count, COUNT(CASE WHEN order_status = 'delivered' THEN o.id ELSE 0 END) AS dcount,  CONCAT_WS('/', DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%b'), DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%y')) AS month"))->groupBy('date', 'months.id')->orderByDesc('date')->get();
+        })->select(DB::raw("LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH AS date, COUNT(o.id) AS order_count, CASE WHEN o.order_status = 'delivered' THEN COUNT(o.id) ELSE 0 END AS dcount,  CONCAT_WS('/', DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%b'), DATE_FORMAT(LAST_DAY(CURRENT_DATE) + INTERVAL 1 DAY - INTERVAL months.id MONTH, '%y')) AS month"))->groupBy('date', 'months.id')->orderByDesc('date')->get();
         return json_encode($orders);
     }
 
-    public function getBranches($id){
-        return Branch::when($id > 0, function($q) use ($id){
+    public function getBranches($id)
+    {
+        return Branch::when($id > 0, function ($q) use ($id) {
             return $q->where('id', $id);
         })->get();
     }
