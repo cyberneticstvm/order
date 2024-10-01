e9c82a9a2424d23c9e0482e803072ecc31a60282
Cybernetics <mail@cybernetics.me>
2024-10-01 16:48:53 +0530

    small updates

--- public/backend/assets/js/dashboard/dashboard.js
+++ public/backend/assets/js/dashboard/dashboard.js
@@ -157,164 +157,165 @@ $(function () {
           chartordercomparison.render();
     });
 
-    getSalesComparisonData(0, 0, 0);      
+    
+
+    $.getJSON('/ajax/chart/sales/comparison/0/0/0', function (response) {
+      //console.log(response.invtot)
+      var options = {
+        labels: ["Advance", "Balance"],
+        series: [parseFloat(response.advance), parseFloat(response.balance)],
+        chart: {
+          type: "donut",
+          height: 300,
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        legend: {
+          position: "bottom",
+          fontSize: "14px",
+          fontFamily: "Rubik, sans-serif",
+          fontWeight: 500,
+          labels: {
+            colors: ["var(--chart-text-color)"],
+          },
+          markers: {
+            width: 6,
+            height: 6,
+          },
+          itemMargin: {
+            horizontal: 7,
+            vertical: 0,
+          },
+        },
+        stroke: {
+          width: 10,
+          colors: ["var(--light2)"],
+        },
+        plotOptions: {
+          pie: {
+            expandOnClick: false,
+            donut: {
+              size: "83%",
+              labels: {
+                show: true,
+                name: {
+                  offsetY: 4,
+                },
+                total: {
+                  show: true,
+                  fontSize: "20px",
+                  fontFamily: "Rubik, sans-serif",
+                  fontWeight: 500,
+                  label: "₹ "+ response.invtot,
+                  formatter: () => "Total Sales",
+                },
+              },
+            },
+          },
+        },
+        states: {
+          normal: {
+            filter: {
+              type: "none",
+            },
+          },
+          hover: {
+            filter: {
+              type: "none",
+            },
+          },
+          active: {
+            allowMultipleDataPointsSelection: false,
+            filter: {
+              type: "none",
+            },
+          },
+        },
+        colors: ["#54BA4A", "#FFA941"],
+        responsive: [
+          {
+            breakpoint: 1630,
+            options: {
+              chart: {
+                height: 360,
+              },
+            },
+          },
+          {
+            breakpoint: 1584,
+            options: {
+              chart: {
+                height: 400,
+              },
+            },
+          },
+          {
+            breakpoint: 1473,
+            options: {
+              chart: {
+                height: 250,
+              },
+            },
+          },
+          {
+            breakpoint: 1425,
+            options: {
+              chart: {
+                height: 270,
+              },
+            },
+          },
+          {
+            breakpoint: 1400,
+            options: {
+              chart: {
+                height: 320,
+              },
+            },
+          },
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                height: 250,
+              },
+            },
+          },
+        ],
+      };
+    var salescomparisonchart = new ApexCharts(
+      document.querySelector("#sales-comparison-chart"),
+      options
+    );
+    salescomparisonchart.render();
+  });      
 
 
     $(".selChangeChart").change(function(e){
         let branch = $("#current_branch").val();
         let month = $("#selChangeMonth").val();
         let year = $("#selChangeYear").val();
-        var options = {
-          chart: {
-              height: 300,
-              type: 'donut',
-          },
-          series: [],
-          noData: {
-            text: 'Loading...'
+        $.ajax({
+          url: '/ajax/chart/sales/comparison/'+branch+'/'+month+'/'+year,
+          type: 'GET',
+          dataType: 'json',
+          async: true,
+          cache: false,
+          //beforeSend: function() { openLoadingDialog('#container'); },
+          success: function (response) {
+              //closeLoadingDialog('#container');
+              console.log(response.invtot)
+              var salescomparisonchart = new ApexCharts(
+                document.querySelector("#sales-comparison-chart"),
+                options
+              );
+              salescomparisonchart.render();
           }
-        }
-        var chart = new ApexCharts(
-          document.querySelector("#sales-comparison-chart"),
-          options
-        );        
-        chart.render();
-
-        getSalesComparisonData(branch, month, year);
-        e.preventDefault();
+      });
     });
 });
 
-function getSalesComparisonData(branch, month, year){
+
   
-  $.getJSON('/ajax/chart/sales/comparison/'+branch+'/'+month+'/'+year, function (response) {
-    var options = {
-      labels: ["Advance", "Balance"],
-      series: [parseFloat(response.data[0].advance), parseFloat(response.data[0].balance)],
-      chart: {
-        type: "donut",
-        height: 300,
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        position: "bottom",
-        fontSize: "14px",
-        fontFamily: "Rubik, sans-serif",
-        fontWeight: 500,
-        labels: {
-          colors: ["var(--chart-text-color)"],
-        },
-        markers: {
-          width: 6,
-          height: 6,
-        },
-        itemMargin: {
-          horizontal: 7,
-          vertical: 0,
-        },
-      },
-      stroke: {
-        width: 10,
-        colors: ["var(--light2)"],
-      },
-      plotOptions: {
-        pie: {
-          expandOnClick: false,
-          donut: {
-            size: "83%",
-            labels: {
-              show: true,
-              name: {
-                offsetY: 4,
-              },
-              total: {
-                show: true,
-                fontSize: "20px",
-                fontFamily: "Rubik, sans-serif",
-                fontWeight: 500,
-                label: "₹ "+ response.data[0].invtot,
-                formatter: () => "Total Sales",
-              },
-            },
-          },
-        },
-      },
-      states: {
-        normal: {
-          filter: {
-            type: "none",
-          },
-        },
-        hover: {
-          filter: {
-            type: "none",
-          },
-        },
-        active: {
-          allowMultipleDataPointsSelection: false,
-          filter: {
-            type: "none",
-          },
-        },
-      },
-      colors: ["#54BA4A", "#FFA941"],
-      responsive: [
-        {
-          breakpoint: 1630,
-          options: {
-            chart: {
-              height: 360,
-            },
-          },
-        },
-        {
-          breakpoint: 1584,
-          options: {
-            chart: {
-              height: 400,
-            },
-          },
-        },
-        {
-          breakpoint: 1473,
-          options: {
-            chart: {
-              height: 250,
-            },
-          },
-        },
-        {
-          breakpoint: 1425,
-          options: {
-            chart: {
-              height: 270,
-            },
-          },
-        },
-        {
-          breakpoint: 1400,
-          options: {
-            chart: {
-              height: 320,
-            },
-          },
-        },
-        {
-          breakpoint: 480,
-          options: {
-            chart: {
-              height: 250,
-            },
-          },
-        },
-      ],
-    };
-  var chart = new ApexCharts(
-    document.querySelector("#sales-comparison-chart"),
-    options
-  );
-  chart.render();
-});
-}
+  
