c56acb35e4d30bfa60ebbe93409cfa42a0bbf634
Cybernetics <mail@cybernetics.me>
2024-10-07 10:44:56 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -55,12 +55,12 @@ class AjaxController extends Controller
     public function getPrescription($source, $val)
     {
         if ($source == 'hospital') :
-            $spectacle = DB::connection('mysql1')->table('spectacles')->leftJoin('users as u', 'u.id', 'spectacles.created_by')->selectRaw("re_dist_sph as re_sph, re_dist_cyl as re_cyl, re_dist_axis as re_axis, re_dist_add as re_add, re_dist_va as re_va, rpd as re_pd, lpd as le_pd, le_dist_sph as le_sph, le_dist_cyl as le_cyl, le_dist_axis as le_axis, le_dist_add as le_add, le_dist_va as le_va, re_int_add, le_int_add, '' as a_size, '' as b_size, '' as dbl, '' as fh, '' as ed, '' as vd, '' as w_angle, 0 as doctor, u.name as optometrist")->where('spectacles.id', $val)->first();
-        /*$secret = apiSecret();
+            /*$spectacle = DB::connection('mysql1')->table('spectacles')->leftJoin('users as u', 'u.id', 'spectacles.created_by')->selectRaw("re_dist_sph as re_sph, re_dist_cyl as re_cyl, re_dist_axis as re_axis, re_dist_add as re_add, re_dist_va as re_va, rpd as re_pd, lpd as le_pd, le_dist_sph as le_sph, le_dist_cyl as le_cyl, le_dist_axis as le_axis, le_dist_add as le_add, le_dist_va as le_va, re_int_add, le_int_add, '' as a_size, '' as b_size, '' as dbl, '' as fh, '' as ed, '' as vd, '' as w_angle, 0 as doctor, u.name as optometrist")->where('spectacles.id', $val)->first();*/
+            $secret = apiSecret();
             $url = api_url() . "/api/prescription/" . $val . "/" . $secret;
             $json = file_get_contents($url);
             $data = json_decode($json);
-            $spectacle = $data->spectacle;*/
+            $spectacle = $data->spectacle;
         else :
             $spectacle = Spectacle::findOrFail($val);
         endif;
