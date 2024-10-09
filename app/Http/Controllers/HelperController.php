333436d1c2ba73eef8b2fcd2106b3e522f726ca3
Cybernetics <mail@cybernetics.me>
2024-10-09 15:03:00 +0530

    small updates

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -388,7 +388,7 @@ class HelperController extends Controller
 
     function asd()
     {
-        $products = getInventory(Session::get('branch'), 0, 'frame')->where('balanceQty', '>', 0);
+        $products = getInventory(Session::get('branch'), 0, 'frame')->where('balanceQty', '>', 0)->pluck('product_name', 'product_id');
         dd($products);
     }
 }
