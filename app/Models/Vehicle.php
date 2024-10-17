248aca3fddc6f67c2dc9ad25ccbede833b072658
Cybernetics <mail@cybernetics.me>
2024-10-17 19:50:52 +0530

    small updates

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -30,7 +30,7 @@ class Vehicle extends Model
 
     public function vstatus()
     {
-        return (Carbon::now()->diffInDays(Carbon::parse($this->payment()?->first()?->created_at)) < $this->payment_terms) ? 'Active' : 'Inactive';
+        return (Carbon::now()->diffInDays(Carbon::parse($this->payment()?->first()?->created_at)) < $this->payment_terms) ? "<span class='badge badge-success'>Active</span>" : "<span class='badge badge-danger'>Inactive</span>";
     }
 
     public function daysLeft()
