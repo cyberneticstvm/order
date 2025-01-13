8f2533abcb983722d91bb99171eafd7b4d0d8800
Cybernetics <mail@cybernetics.me>
2025-01-13 17:03:42 +0530

    small updates

--- app/Http/Controllers/AdvertisementController.php
+++ app/Http/Controllers/AdvertisementController.php
@@ -107,7 +107,8 @@ class AdvertisementController extends Controller
         $fee = $vehicle->fee;
         if ($days < 0) {
             $days = abs($days);
-            $fee = $fee + (($fee * 30) / 100) * $days;
+            $tot = $fee + (($fee * 30) / 100) * $days;
+            $fee = $tot;
         }
         return view('backend.ads.vehicle.payment', compact('vehicle', 'diff', 'payments', 'pmodes', 'fee'));
     }
