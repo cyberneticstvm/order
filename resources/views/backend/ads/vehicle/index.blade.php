f415926c101bdaad4ed23937003007320c38e8cf
Cybernetics <mail@cybernetics.me>
2025-01-13 16:53:26 +0530

    small updates

--- resources/views/backend/ads/vehicle/index.blade.php
+++ resources/views/backend/ads/vehicle/index.blade.php
@@ -79,7 +79,7 @@
                                         <td>{{ $vehicle->fee }}</td>
                                         <td>{{ $vehicle->payment_terms }} Days</td>
                                         <td class="text-success">{{ $vehicle->daysLeft() }} Days</td>
-                                        <td>{{ $vehicle->payment->first()->created_at->format('d.M.Y') }}</td>
+                                        <td>{{ $vehicle->payment?->first()?->created_at?->format('d.M.Y') }}</td>
                                         <td class="text-center">{!! $vehicle->vstatus() !!}</td>
                                         <td>{{ $vehicle->card_issued == 0 ? 'No' : 'Yes' }} - {{ $vehicle->card_issued_date?->format('d.M.Y') }}</td>
                                         <td class="text-center">{!! $vehicle->status() !!}</td>
