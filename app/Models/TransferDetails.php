76db476b6bee3583d64f8398f1acb408badc7d4e
Cybernetics <mail@cybernetics.me>
2025-04-24 15:36:02 +0530

    small updates

--- app/Models/TransferDetails.php
+++ app/Models/TransferDetails.php
@@ -11,6 +11,8 @@ class TransferDetails extends Model
 
     protected $guarded = [];
 
+    protected $casts = ['transfer_date' => 'datetime', 'accepted_at' => 'datetime', 'created_at' => 'datetime'];
+
     public function product()
     {
         return $this->belongsTo(Product::class, 'product_id', 'id');
