22464292b4f16974399ff8f5fe08108b6ee93c9f
Cybernetics <mail@cybernetics.me>
2025-04-24 17:12:44 +0530

    small updates

--- app/Http/Controllers/TransferFrameController.php
+++ app/Http/Controllers/TransferFrameController.php
@@ -41,10 +41,12 @@ class TransferFrameController extends Controller
                 return $q->union($brs);
             })->orderBy('id')->pluck('name', 'id');
 
+            $this->products = getInventory(Session::get('branch'), 0, 'frame')->where('balanceQty', '>', 0);
+
             return $next($request);
         });
 
-        $this->products = Product::whereIn('category', ['frame'])->selectRaw("id, CONCAT_WS('-', name, code) AS name")->orderBy('name')->pluck('name', 'id');
+        //$this->products = Product::whereIn('category', ['frame'])->selectRaw("id, CONCAT_WS('-', name, code) AS name")->orderBy('name')->pluck('name', 'id');
     }
 
     public function index()
