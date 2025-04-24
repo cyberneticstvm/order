76d60a9a4988f441f2f392a605cd80db734b07f4
Cybernetics <mail@cybernetics.me>
2025-04-24 17:07:39 +0530

    small updates

--- app/Http/Controllers/SolutionOrderController.php
+++ app/Http/Controllers/SolutionOrderController.php
@@ -59,8 +59,6 @@ class SolutionOrderController extends Controller
     public function create($id, $type)
     {
         $products = $this->products;
-        dd($products);
-        die;
         $pmodes = $this->pmodes;
         $padvisers = $this->padvisers;
         $states = State::all();
