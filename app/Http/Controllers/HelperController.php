53bf527e2507dfe8413697aa53e68bf5692602cb
Cybernetics <mail@cybernetics.me>
2025-05-02 18:28:30 +0530

    small updates

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -504,9 +504,9 @@ class HelperController extends Controller
 
     function asd()
     {
-        /*$products = getInventory(Session::get('branch'), 0, 'frame')->where('balanceQty', '>', 0);
-        dd($products);*/
-        $promo = PromotionSchedule::whereDate('scheduled_date', Carbon::today())->where('status', 'publish')->latest()->first();
+        $products = getInventory(Session::get('branch'), 0, 'frame')->where('balanceQty', '>', 0);
+        dd($products);
+        /*$promo = PromotionSchedule::whereDate('scheduled_date', Carbon::today())->where('status', 'publish')->latest()->first();
         if ($promo):
             $clist = PromotionContact::selectRaw("id, name, contact_number as mobile, 'clist' as type")->whereNull('wa_sms_status')->where('entity', $promo->entity)->where('type', 'include')->when($promo->branch_id > 0, function ($q) use ($promo) {
                 return $q->where('branch_id', $promo->branch_id);
@@ -531,6 +531,6 @@ class HelperController extends Controller
                 PromotionContact::whereIn('id', $ids1)->update(['wa_sms_status' => 'yes']);
                 Order::whereIn('id', $ids2)->update(['wa_sms_status' => 'yes']);
             endif;
-        endif;
+        endif;*/
     }
 }
