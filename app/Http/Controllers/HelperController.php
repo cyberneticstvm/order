774cc0426fd96070cc3a96069469b40f40147963
Cybernetics <mail@cybernetics.me>
2025-05-02 17:13:35 +0530

    small updates

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -533,6 +533,6 @@ class HelperController extends Controller
             endif;
         endif;*/
         $promo = PromotionSchedule::whereDate('scheduled_date', Carbon::today())->where('status', 'publish')->latest()->first();
-        sendWaPromotion($promo, 'Vijo', '9188848860');
+        dd($promo);
     }
 }
