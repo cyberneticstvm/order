bd798909e1f53822ab052580a289d32042d8e810
Cybernetics <mail@cybernetics.me>
2025-05-16 19:57:52 +0530

    small updates

--- resources/views/backend/order/store/edit.blade.php
+++ resources/views/backend/order/store/edit.blade.php
@@ -379,6 +379,10 @@
                                                     <td colspan="11" class="text-end fw-bold border-0">Balance</td>
                                                     <td class="border-0"><input type="text" name="balance" class="text-end border-0 fw-bold w-100 balance readOnly" placeholder="0.00" value="{{ number_format($order->balance + $order->credit_used, 2, '.', '') }}" /></td>
                                                 </tr>
+                                                <tr>
+                                                    <td colspan="11" class="text-end fw-bold border-0">Paid</td>
+                                                    <td class="border-0"><input type="text" name="balance" class="text-end border-0 fw-bold w-100 balance readOnly" placeholder="0.00" value="{{ number_format($order->invoice_total - ($order->payments->sum('amount') + $order->credit_used), 2) }}" /></td>
+                                                </tr>
                                             </tfoot>
                                         </table>
                                     </div>
