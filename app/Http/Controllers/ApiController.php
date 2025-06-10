bf79fce6bd68c304b78e289ae5465b45f477f3d5
Cybernetics <mail@cybernetics.me>
2025-06-10 15:07:56 +0530

    small updates

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -71,7 +71,7 @@ class ApiController extends Controller
     function getOrders($secret)
     {
         if ($secret == apiSecret()) :
-            $orders = Order::leftJoin('branches AS b', 'b.id', 'orders.branch_id')->selectRaw("orders.id, b.code, orders.name AS customer, orders.place, DATE_FORMAT(orders.order_date, '%d.%M.%Y') AS odate")->whereDate('orders.order_date', Carbon::today())->latest()->get();
+            $orders = Order::leftJoin('branches AS b', 'b.id', 'orders.branch_id')->selectRaw("orders.id, b.code, orders.name AS customer, orders.place, DATE_FORMAT(orders.order_date, '%d.%M.%Y') AS odate")->whereDate('orders.order_date', Carbon::today())->orderBy('orders.branch_id')->get();
             return response()->json([
                 'status' => true,
                 'data' => $orders,
