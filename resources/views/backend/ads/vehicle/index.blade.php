b3aac61e93de743aa9ed9619d0dd551ca90a25aa
Cybernetics <mail@cybernetics.me>
2025-06-24 08:04:49 +0530

    small updates

--- resources/views/backend/ads/vehicle/index.blade.php
+++ resources/views/backend/ads/vehicle/index.blade.php
@@ -82,7 +82,7 @@
                                         <td>{{ $vehicle->payment?->first()?->created_at?->format('d.M.Y') }}</td>
                                         <td class="text-center">{!! $vehicle->vstatus() !!}</td>
                                         <td>{{ $vehicle->card_issued == 0 ? 'No' : 'Yes' }} - {{ $vehicle->card_issued_date?->format('d.M.Y') }}</td>
-                                        <td><a href="javascript:void(0)" class="payment" data-ono="{{ $vehicle->id }}" data-type="payment" data-oid="{{ $vehicle->id }}" data-branch="" data-drawer="paymentDrawer"><i class="fa fa-qrcode text-success"></i></a></td>
+                                        <td><a href="javascript:void(0)" class="payment" data-ono="{{ $vehicle->id }}" data-type="payment" data-oid="{{ $vehicle->id }}" data-branch="" data-drawer="paymentDrawer"><i class="fa fa-qrcode fa-xl text-success"></i></a></td>
                                         <td class="text-center">{!! $vehicle->status() !!}</td>
                                         <td class="text-center"><a href="{{ route('vehicle.edit', encrypt($vehicle->id)) }}"><i class="fa fa-edit text-muted fa-lg"></i></a></td>
                                         <td class="text-center">
