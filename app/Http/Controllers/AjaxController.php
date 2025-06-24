93503720d7abec768761e5ee9a7b37be4993eadd
Cybernetics <mail@cybernetics.me>
2025-06-24 09:08:08 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -674,7 +674,7 @@ class AjaxController extends Controller
 
     public function generatePaymentQr(Request $request)
     {
-        $vehicle = Vehicle::findOrFail($request->vehicle_id);
+        $vehicle = Vehicle::findOrFail($request->vid);
         $pn = $vehicle->reg_number;
         $tn = $request->mobile;
         $am = $vehicle->totalCredit();
