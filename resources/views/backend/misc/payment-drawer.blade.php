77b97c8178f466f5888cacb51219fde3fb6a9438
Cybernetics <mail@cybernetics.me>
2025-06-24 10:44:34 +0530

    small updates

--- resources/views/backend/misc/payment-drawer.blade.php
+++ resources/views/backend/misc/payment-drawer.blade.php
@@ -20,8 +20,8 @@
                         <button class="btn btn-generate-qr btn-success">Generate</button>
                     </div>
                 </form>
-                <div class="col-md-12 qrCode" id="qrCode">
-
+                <div class="col-md-12">
+                    <canvas id="qrCode"></canvas>
                 </div>
             </div>
         </div>
