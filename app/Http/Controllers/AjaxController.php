2954d8cc8f8ec4159da2d90d82c99477e25d60d1
Cybernetics <mail@cybernetics.me>
2025-07-01 16:22:50 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -680,7 +680,10 @@ class AjaxController extends Controller
         $upi = ($vehicle->upi_id) ? $vehicle->upi_id : $request->mobile . '@upi';
         $days = $vehicle->daysLeft();
         $am = ($days < 0) ? $vehicle->fee + ($vehicle->fee / 30) * abs($days) : $vehicle->fee - ($vehicle->fee / 30) * abs($days);
-        $qr = base64_encode(QrCode::format('svg')->size(150)->color(0, 128, 0)->errorCorrection('H')->generate('upi://pay?pa=' . $upi . '&pn=' . $pn . '&tn=' . $tn . '&am=' . ceil($am) . '&cu=INR'));
+        $color = '#ff3300';
+        $RGBarray = sscanf($color, "#%02x%02x%02x");
+        $RGBcolour = join(',', $RGBarray);
+        $qr = base64_encode(QrCode::format('svg')->size(150)->color($RGBcolour)->errorCorrection('H')->generate('upi://pay?pa=' . $upi . '&pn=' . $pn . '&tn=' . $tn . '&am=' . ceil($am) . '&cu=INR'));
         return response()->json([
             'qrCode' => $qr,
         ]);
