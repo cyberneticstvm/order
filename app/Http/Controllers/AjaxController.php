e6360280ec50496a9a569256398d4d04910c446e
Cybernetics <mail@cybernetics.me>
2025-07-01 16:08:49 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -680,7 +680,7 @@ class AjaxController extends Controller
         $upi = ($vehicle->upi_id) ? $vehicle->upi_id : $request->mobile . '@upi';
         $days = $vehicle->daysLeft();
         $am = ($days < 0) ? $vehicle->fee + ($vehicle->fee / 30) * abs($days) : $vehicle->fee - ($vehicle->fee / 30) * abs($days);
-        $color = ($vehicle->upi_id) ? "0, 128, 0" : "255, 0 , 0";
+        $color = ($vehicle->upi_id) ? array(0, 128, 0) : array(255, 0, 0);
         $qr = base64_encode(QrCode::format('svg')->size(150)->color($color)->errorCorrection('H')->generate('upi://pay?pa=' . $upi . '&pn=' . $pn . '&tn=' . $tn . '&am=' . ceil($am) . '&cu=INR'));
         return response()->json([
             'qrCode' => $qr,
