f13131fda8a88022c562f6cb7af596b11f2ca06b
Cybernetics <mail@cybernetics.me>
2025-07-11 17:16:51 +0530

    small updates

--- resources/views/backend/order/return/index.blade.php
+++ resources/views/backend/order/return/index.blade.php
@@ -73,8 +73,8 @@
                                     <tr>
                                         <td>{{ $key + 1 }}</td>
                                         <td><a href="{{ route('sales.return.list.detail', encrypt($item->id)) }}">{{ $item->id }}</a></td>
-                                        <td></td>
                                         <td>{{ $item->order->branch->code }}/{{ $item->order->id }}</td>
+                                        <td>{{ $item->products($item->details->pluck('product_id')) }}</td>
                                         <td>{{ $item->orderBranch->name }}</td>
                                         <td>{{ $item->returnBranch->name }}</td>
                                         <td>{{ $item->order->name }}</td>
