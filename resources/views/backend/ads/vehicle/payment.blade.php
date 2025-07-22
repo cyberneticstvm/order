30fc153553de74acbb03a7a0855ba407d515a043
Cybernetics <mail@cybernetics.me>
2025-07-22 17:40:14 +0530

    small updates

--- resources/views/backend/ads/vehicle/payment.blade.php
+++ resources/views/backend/ads/vehicle/payment.blade.php
@@ -48,9 +48,7 @@
                                 </div>
                                 <div class="col-md-3">
                                     <label class="form-label">UPI Id</label>
-                                    {{ html()->text($name = 'upi_id', $vehicle->upi_id ?? null)->class('form-control')->maxLength(50)->placeholder('UPI Id')->if($vehicle->upi_id != '', function($el){
-                                        return $el->attribute('readonly', true);
-                                    }) }}
+                                    {{ html()->text($name = 'upi_id', $vehicle->upi_id ?? null)->class('form-control')->maxLength(50)->placeholder('UPI Id') }}
                                     @error('upi_id')
                                     <small class="text-danger">{{ $errors->first('upi_id') }}</small>
                                     @enderror
