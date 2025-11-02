08b2a9a66f4fcee0b3e04cef5316e32a42b39672
Cybernetics <mail@cybernetics.me>
2025-11-02 10:52:48 +0530

    Small Updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -165,6 +165,8 @@ class ReportController extends Controller
             'branch' => 'required',
         ]);
         $data = getInventory($request->branch, 0, $request->category);
+        dd($data);
+        die;
         $branches = $this->branches;
         $inputs = array($request->branch, $request->category);
         return view('backend.report.stock', compact('data', 'branches', 'inputs'));
