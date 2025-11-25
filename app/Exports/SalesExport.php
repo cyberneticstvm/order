097129a2057552916bc60be85ad0414d9748ca6f
Cybernetics <mail@cybernetics.me>
2025-11-25 15:30:15 +0530

    small updates

--- app/Exports/SalesExport.php
+++ app/Exports/SalesExport.php
@@ -26,11 +26,9 @@ class SalesExport implements FromCollection, WithHeadings, ShouldAutoSize, WithS
         $request = $this->request;
         $sales = Order::whereBetween(($request->status != 'delivered') ? 'order_date' : 'invoice_generated_at', [Carbon::parse($request->from_date)->startOfDay(), Carbon::parse($request->to_date)->endOfDay()])->when($request->branch > 0, function ($q) use ($request) {
             return $q->where('branch_id', $request->branch);
-        })->when($request->order_status != 'all', function ($q) use ($request) {
-            return $q->where('order_status', $request->order_status);
+        })->when($request->status != 'all', function ($q) use ($request) {
+            return $q->where('order_status', $request->status);
         })->orderBy('order_sequence', 'ASC')->get();
-        dd($sales);
-        die;
 
         return $sales->map(function ($data, $key) {
             return [
