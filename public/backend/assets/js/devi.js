5029d768f938af6f8eb0d307f67cc83c912b08b5
Cybernetics <mail@cybernetics.me>
2025-11-30 10:15:49 +0530

    small updates

--- public/backend/assets/js/devi.js
+++ public/backend/assets/js/devi.js
@@ -864,7 +864,7 @@ function calculatePurchaseTotal() {
         let taxp = parseInt(pdct.split('-').pop());
         var qty = parseInt(dis.find(".pQty").val()); var mrp = parseFloat(dis.find(".pMrp").val());
         var purchase_price = parseFloat(dis.find(".pPPrice").val()); var sales_price = parseFloat(dis.find(".pSPrice").val()); var pTotal = parseFloat(dis.find(".pTotal").val());
-        let discount = parseFloat(dis.find(".discount").val());
+        let discount = parseFloat(dis.find(".discount").val() ?? 0);
         let taxAmount = parseFloat((((purchase_price - discount)*taxp)/100)*qty).toFixed(2);
         dis.find(".taxAmount").val(taxAmount)
         var total = parseFloat(qty * purchase_price);
