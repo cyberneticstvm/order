9cf23add18d650a2c4288a53ee464495eed9f3ee
Cybernetics <mail@cybernetics.me>
2025-12-01 15:04:26 +0530

    small updates

--- resources/views/backend/pdf/purchase.blade.php
+++ resources/views/backend/pdf/purchase.blade.php
@@ -8,10 +8,10 @@
 <div class="row">
     <div class="col">
         <h4 class="text-center">Purchase Details</h4>
-        <table class="table border-0" width="100%" cellpadding="0" cellspacing="0" style="font-size: 10px;">
+        <table class="table" width="100%" cellpadding="0" cellspacing="0" style="font-size: 10px;">
             <tr>
-                <td>Bill No: {{ $purchase->purchase_invoice_number }}</td>
-                <td>Branch: {{ $purchase->branch->name }}</td>
+                <td class="border-0">Bill No: {{ $purchase->purchase_invoice_number }}</td>
+                <td class="border-0">Branch: {{ $purchase->branch->name }}</td>
             </tr>
         </table>
     </div>
