c70b26972984b9fc97338fe4095cf18a647baa0f
Cybernetics <mail@cybernetics.me>
2026-03-06 10:17:57 +0530

    small updates

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -734,9 +734,7 @@ class AjaxController extends Controller
                 foreach ($products->where("balanceQty", ">", 0) as $key => $item):
                     $purchase = PurchaseDetail::where("product_id", $item->id)->latest()->first();
                     if ($purchase):
-                        $data .= "<input type='hidden' name='rtype_ids[]' value='" . $purchase?->id . "' />";
-                        $data .= "<input type='hidden' name='supplier_ids[]' value='" . $purchase?->purchase?->supplier?->id . "' />";
-                        $data .= "<input type='hidden' name='product_ids[]' value='" . $item->product->id . "' />";
+
                         $data .= "<tr>";
                         $data .= "<td>" . $slno . "</td>";
                         $data .= "<td>" . $purchase?->purchase?->purchase_number ?? '' . "</td>";
